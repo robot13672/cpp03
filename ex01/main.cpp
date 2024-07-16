c109c20a2c24364dc79d98ec6123dd01ff5bd8e6
Illia Khrystynchenko <ikhristi@student.42wolfsburg.de>
2024-07-16 13:51:59 +0200

    Final

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,16 +6,31 @@
 /*   By:  ikhristi <ikhristi@student.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 14:20:02 by  ikhristi         #+#    #+#             */
-/*   Updated: 2024/06/17 19:57:15 by  ikhristi        ###   ########.fr       */
+/*   Updated: 2024/07/16 13:00:48 by  ikhristi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
 int main()
 {
-    ScavTrap scavtrap("Vlad");
+    // Создаем объект ClapTrap и тестируем его функции
+    ClapTrap claptrap("Player 1");
+    std::cout << "\n--- Testing ClapTrap ---\n";
+    claptrap.attack("enemy");
+    claptrap.takeDamage(5);
+    claptrap.beRepaired(3);
+    claptrap.takeDamage(10); // Этот вызов должен показать, что ClapTrap мертв
+    claptrap.beRepaired(5);  // Этот вызов должен показать, что мертвый ClapTrap не может быть отремонтирован
 
+    // Создаем объект ScavTrap и тестируем его функции
+    std::cout << "\n--- Testing ScavTrap ---\n";
+    ScavTrap scavtrap("Player 2");
     scavtrap.attack("Illia");
+    scavtrap.takeDamage(50);
+    scavtrap.beRepaired(30);
+    scavtrap.guardGate(); // Тестируем специальную способность guardGate
     
+    return 0;
 }
